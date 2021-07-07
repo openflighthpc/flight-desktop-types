a49ef01d03f240819e5b7a0a8cb7758623079c4d
William McCumsite <william.mccumstie@gmail.com>
2021-07-07 13:04:36 +0100

    Add a script to open apps in the gnome-terminal

---
+++ gnome/launch-app.sh
@@ -0,0 +1,32 @@
+# =============================================================================
+# Copyright (C) 2021-present Alces Flight Ltd.
+#
+# This file is part of Flight Desktop.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Desktop is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Desktop. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Desktop, please visit:
+# https://github.com/alces-flight/flight-desktop
+# ==============================================================================
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export DISPLAY="$1"
+# NOTE: $2 is the index of the current application being launched and
+#       $3 is the total number of apps being launched
+gnome-terminal -- "$DIR"/background-app "${@:4}"
